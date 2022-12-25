118495c92bc24525743e28469bbddf8b7a8bb324
qiwihui <qwh005007@gmail.com>
2022-12-25 21:54:12 +0800

    fix: add landing page

--- src/components/NavBar.tsx
+++ src/components/NavBar.tsx
@@ -1,4 +1,5 @@
 import Image from "next/image";
+import Link from "next/link";
 import { NavItem } from "./NavItem";
 import { AptosConnect } from "./AptosConnect";
 import {
@@ -9,13 +10,16 @@ export function NavBar() {
   return (
     <nav className="navbar py-4 px-4">
       <div className="flex-1 text-lg font-semibold">
-        <a href="#" target="_blank">
+        <Link href="/">
           {/* <Image src="/logo.png" width={64} height={64} alt="logo" /> */}
-          <div className="title cursor-pointer" style={{fontSize: "24px"}}>Wolf Game</div>
-        </a>
+          <div className="title cursor-pointer" style={{ fontSize: "24px" }}>Wolf Game</div>
+        </Link>
         <ul className="menu menu-horizontal p-0 ml-5">
           <li className="font-sans text-lg">
-            <a href={MODULE_URL} target="_blank">Risky Game</a>
+            <Link href='/game'>Play</Link>
+          </li>
+          <li className="font-sans text-lg">
+            <Link href={MODULE_URL} target="_blank">Risky Game</Link>
           </li>
         </ul>
       </div>
